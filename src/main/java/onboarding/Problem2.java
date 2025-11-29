4141487a1ed81a56116260de0e1fbbd5ed24c768
rlagudwls5518 <rlagudwls5518@naver.com>
2025-11-29 21:05:19 +0900

    feat: 문제 2번 돌아가는코드 완성

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,28 @@
 package onboarding;
 
+import java.util.Stack;
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = calculate(cryptogram);
+        return answer;
+    }
+    private static String calculate(String cryptogram) {
+        String answer = "";
+        Stack<Character> tower = new Stack<>();
+        tower.push(cryptogram.charAt(0));
+
+        for (int i = 1; i < cryptogram.length(); i++) {
+            if (tower.peek() == cryptogram.charAt(i)) {
+                tower.pop();
+            }
+            else  {
+                tower.push(cryptogram.charAt(i));
+            }
+        }
+
+        for(char c : tower) {
+            answer += c;
+        }
         return answer;
     }
 }
